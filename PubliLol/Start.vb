68faa80065be507c39e071c1246fb07b62022fc0
Ludlulu <62658994+LudoLud@users.noreply.github.com>
2020-05-20 11:13:00 -0400

    Add files via upload

---
+++ PubliLol/Start.vb
@@ -0,0 +1,35 @@
+﻿Public Class Start
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Visible = False
+        Button2.Visible = False
+        Me.ShowInTaskbar = False
+        Label1.Visible = False
+        basic.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub Start_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Start_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If e.CloseReason = CloseReason.UserClosing Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        End
+    End Sub
+
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
+        Dim ask As MsgBoxResult = MsgBox("Quitter PubliLol?", MsgBoxStyle.YesNo, "Quitter PubliLol")
+        If ask = MsgBoxResult.Yes Then
+            End
+
+        End If
+    End Sub
+End Class
---
+++ PubliLol/poids.vb
@@ -0,0 +1,18 @@
+﻿Public Class poids
+    Private Sub poids_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub poids_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If e.CloseReason = CloseReason.UserClosing Then
+            e.Cancel = True
+            Dim wow7 As New wow
+            wow7.Show()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim basic6 As New basic
+        basic6.Show()
+    End Sub
+End Class
